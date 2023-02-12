8875d721c3f992d6eae7c8feaf7765eaf924f508
mohanlav <kavimohan2498>
2023-02-12 11:14:09 +0530

    project

---
+++ src/test/java/org/kavi/testapp.java
@@ -0,0 +1,16 @@
+package org.kavi;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testapp {
+public static void main(String[] args) {
+	
+	
+	driver.get("https://www.facebook.com/");
+}
+}
